102d4f08ff9ca5a04fe0a3051635746334f5de2f
SvitlanaChudak <svit.lemon@gmail.com>
2023-02-21 15:21:37 +0200

    Update slice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,15 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
     name: 'contacts',
-    initialState: {
-        items: [
+    initialState: [
             { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
             { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
             { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
             { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
         ],
-        filter: '',
-    },
     reducers: {
         addContact(state, action) {
             state.items.push(action.payload);
@@ -18,14 +15,8 @@ const contactsSlice = createSlice({
         deleteContact(state, action) {
             return state.items.filter(item => item.id !== action.payload);
         },
-        setFilter(state, action) {
-            state.filter = action.payload;
-        },
     }
 })
 
-export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
+export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
-
-export const getContacts = state => state.contacts.items;
-export const getFilter = state => state.contacts.filter;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './slice';
+import { contactsReducer, filterReducer } from './filterSlice';
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
+        filter: filterReducer,
     },
 });
